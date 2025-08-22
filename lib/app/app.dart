812c30e8aaa85cbe9d857292eddff4041a51ad4a
AndreiBozu <andreib@nextlogic.ro>
2025-08-22 18:34:54 +0300

    - added icons and titles

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -7,6 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      title: "Andrei Bozu",
       routerConfig: AppRouter.router,
       debugShowCheckedModeBanner: true,
     );
